f107c2cd7bcb44cb6859780cebaae2d3c7d368a5
tejaabp <tejaabp@gmail.com>
2024-09-12 10:10:37 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
-import { ThemeProvider } from '@mui/material/styles';
 
 
 function App() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,50 +2,51 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import { InputAdornment, InputBase, TextField, SearchIcon } from '@mui/material';
-import { Search, SentimentDissatisfied } from "@mui/icons-material";
+import { InputBase } from '@mui/material';
+import { Search } from "@mui/icons-material";
 import logo from '../assets/logo.png'
 import ButtonComponent from './ButtonComponent';
 
 export default function Navbar() {
   return (
-    <Box sx={{ fontFamily: 'Poppins' }}>
-      <AppBar position="static">
-        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Box
-            size="large"
-            edge="start"
-            sx={{ mr: 2 }}
+    <AppBar position="static">
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        {/* logo */}
+        <Box component='div'
+          size="large"
+          edge="start"
+          sx={{ mr: 2 }}
           >
-            <img src={logo} alt='logo' />
-          </Box>
-          <Box component="div"
-            size="small"
+          <img src={logo} alt='logo' className='navbar-logo' />
+        </Box>
+        {/* Search bar */}
+        <Box component="div"
+          size="small"
+          sx={{ display: 'flex',
+            alignItems: 'center',
+            backgroundColor: 'primary.light',
+            color: 'primary.dark',
+            border: '1px solid',
+            borderColor: 'primary.dark',
+            borderRadius: '5px'
+          }}>
+          <InputBase
             sx={{
-              backgroundColor: 'primary.light',
               color: 'primary.dark',
-              border: '1px solid',
-              borderColor: 'primary.dark',
-              borderRadius: '5px'
-            }}>
-            <InputBase
-              sx={{
-                ml: 1, p: 0.5, flex: 1, borderRight: '1px solid #111', width: '15rem', textAlign: 'left'
-              }}
-              placeholder="Search a album od your choice"
-              inputProps={{ "aria-label": "search a album od your choice" }}
-            />
-            <IconButton type="button" aria-label="search">
-              <Search color="primary.dark" />
-            </IconButton>
-          </Box>
-          <ButtonComponent value={'Give Feedback'} />
-        </Toolbar>
-      </AppBar>
-    </Box >
+              ml: 1, p: 0.5, flex: 1, borderRight: '1px solid #111', width: '15rem', textAlign: 'left'
+            }}
+            placeholder="Search a album od your choice"
+            inputProps={{ "aria-label": "search a album od your choice" }}
+          />
+          <IconButton type="button" aria-label="search">
+            <Search color="primary.dark" />
+          </IconButton>
+        </Box>
+        {/* feebback button */}
+        <ButtonComponent value={'Give Feedback'} />
+      </Toolbar>
+    </AppBar>
   );
 }
 
